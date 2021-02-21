e8448c6617403c8eab04e9815661a0c480a031c8
anton schmidt <annton96@gmx.de>
2021-02-21 20:38:36 +0100

    initial commit

---
+++ ConsoleApp1/Auto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Auto
+    {
+
+        // Attribute
+        private string form;
+        private string hupton;
+        private int preis;
+        private int geschwindigkeit;
+        private readonly int schrittgeschwindigkeit = 7;
+        // gangschaltung
+
+        // Konstruktor
+        public Auto(string form, int preis)
+        {
+            this.form = form;
+            this.preis = preis;
+        }
+
+        public Auto(int preis)
+        {
+            this.form = "SUV";
+            this.preis = preis;
+        }
+
+        // Methoden
+        public void Fahr()
+        {
+            Console.WriteLine("Ich fahre gerade:" + geschwindigkeit);
+        }
+
+        public void Hupe()
+        {
+            Console.WriteLine("Ich hupe: " + hupton);
+        }
+
+        public void Schrittgeschwindigkeit()
+        {
+            Console.WriteLine("Ich fahre jetzt Schrittgeschwindigkeit.");
+
+            int bremsung = geschwindigkeit - schrittgeschwindigkeit;
+
+            Console.WriteLine("Bremsung: " + bremsung + "km/h");
+
+            SetGeschwindigkeit(schrittgeschwindigkeit);
+        }
+
+        public int GetPreis()
+        {
+            return this.preis;
+        }
+
+        public string GetForm()
+        {
+            return this.form;
+        }
+
+        public void SetHupton(string hupton) // Setter gibt nie etwas zurück 
+        {
+            this.hupton = hupton;
+        }
+
+        public void SetGeschwindigkeit(int geschwindigkeit)
+        {
+            this.geschwindigkeit = geschwindigkeit;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto01 = new Auto("SUV", 20000);
+            Auto auto02 = new Auto(25000);
+            
+            
+            Console.WriteLine("Jetzt kommt Auto 01:");
+            auto01.SetGeschwindigkeit(130);
+            auto01.Fahr();
+            auto01.SetHupton("heee");
+            auto01.Hupe();
+            
+            Console.WriteLine("Jetzt kommt Auto 02:");
+            auto02.SetGeschwindigkeit(100); 
+            auto02.Fahr();
+            auto02.SetHupton("hooo");
+            auto02.Hupe();
+            auto02.SetHupton("hiii");
+            auto02.Hupe();
+            auto02.SetGeschwindigkeit(120);
+            auto02.Fahr();
+            auto02.Schrittgeschwindigkeit();
+            auto02.Fahr();
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
